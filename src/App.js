698f4eab85ea37b4ad7f889b0827ea86c8769d4d
Cansu <canbugref@gmail.com>
2022-10-03 16:15:56 +0300

    Add React Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [calculator, setCalculator] = useState("");
+  const [times, setTimes] = useState("");
+
+  const addClick = (e) => {
+    setCalculator(calculator.concat(e.target.value));
+  };
+
+  const clear = () => {
+    setCalculator("");
+  };
+
+  const clearMinus = (e) => {
+    setCalculator(calculator.slice(0, calculator.length - 1));
+  };
+
+  const calculate = () => {
+    setCalculator(eval(calculator).toString());
+  };
+
+  function formatTime(elem) {
+    if (elem < 10) {
+      return "0";
+    } else {
+      return "";
+    }
+  }
+
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    const date = new Date();
+    const h = date.getHours();
+    const m = date.getMinutes();
+
+    setTimes(formatTime(h) + h + ":" + formatTime(m) + m);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <div className="input">
+          <div className="time">{times}</div>
+          <div className="day">{new Date().toLocaleDateString()}</div>
+        </div>
+
+        <input type="text" className="inputCalculator" value={calculator} />
+      </div>
+      <div className="calculator">
+        <button value="C" onClick={clearMinus} id="clear" className="equals">
+          C
+        </button>
+        <button value="AC" onClick={clear} id="ac" className="equals">
+          AC
+        </button>
+        <button value="/" onClick={addClick} className="equals">
+          /
+        </button>
+        <button value="7" onClick={addClick}>
+          7
+        </button>
+        <button value="8" onClick={addClick}>
+          8
+        </button>
+        <button value="9" onClick={addClick}>
+          9
+        </button>
+        <button value="*" onClick={addClick} className="equals">
+          *
+        </button>
+        <button value="4" onClick={addClick}>
+          4
+        </button>
+        <button value="5" onClick={addClick}>
+          5
+        </button>
+        <button value="6" onClick={addClick}>
+          6
+        </button>
+        <button value="-" onClick={addClick} className="equals">
+          -
+        </button>
+        <button value="3" onClick={addClick}>
+          3
+        </button>
+        <button value="2" onClick={addClick}>
+          2
+        </button>
+        <button value="1" onClick={addClick}>
+          1
+        </button>
+        <button value="+" onClick={addClick} className="equals">
+          +
+        </button>
+        <button value="0" onClick={addClick}>
+          0
+        </button>
+        <button value="." onClick={addClick} className="equals">
+          .
+        </button>
+        <button value="=" onClick={calculate} id="equals">
+          =
+        </button>
+      </div>
     </div>
   );
 }
